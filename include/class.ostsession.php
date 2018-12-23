4075c7c0012aae8a4ade0c2a0341578ec0219b3c
Michael Reed <villascape@gmail.com>
2018-12-23 16:48:24 +0000

    Fix to allow sessions to work with PHP7

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -201,6 +201,9 @@ extends SessionBackend {
         catch (OrmException $e) {
             return false;
         }
+        if(!is_string($this->data->session_data)) {
+            $this->data->session_data = strval($this->data->session_data);
+        }
         return $this->data->session_data;
     }
 
